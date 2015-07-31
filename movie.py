b5543196984d39293c35473f3624ea09b9e9b2e9
Bogdan Gersak <bogdan@gersak.ro>
2015-07-31 21:36:53 +0000

    Add comments

--- movie.py
+++ movie.py
@@ -1,6 +1,19 @@
 class Movie():
-    def __init__(self, title, storyline, poster_image, trailer_youtube):
+    """Basic Movie class representation.
+
+    Attributes:
+        title: Movie title.
+        year: Released year.
+        storyline: Movie short description.
+        poster_image: URL to movie poster.
+        trailer_youtube: URL to movie trailer.
+    """
+    
+    def __init__(self, title, year, storyline, poster_image, trailer_youtube):
+        """Inits Movie class with title, year, storyline, poster and trailer."""
+
         self.title = title
+        self.year = year
         self.storyline = storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
--- server.py
+++ server.py
@@ -2,14 +2,36 @@ import json
 import movie
 import app
 
-# Get movies details from json file
-json_data = open('./data/movies.json').read();
-movies_data = json.loads(json_data)
+def get_movies(filename):
+    """Fetches movie information from a file.
 
-# Create the list of movies
-movies = []
-for m in movies_data:
-    movies.append(movie.Movie(m["title"], m["storyline"], m["poster_image_url"], m["trailer_youtube_url"]))
+    Retrieves movies details from the file given as parameter.
 
-# Open movies page
-app.open_page(movies)
+    Args:
+        filename: A JSON file which contains the required movie information:
+            title, year, story line, poster image url, trailer youtube url
+
+    Returns:
+        A list of Movie class instances
+    """
+
+    # Get movies details from the json file
+    json_data = open(filename).read();
+    movies_data = json.loads(json_data)
+
+    # Create the list of movies
+    movies = []
+    for m in movies_data:
+        movies.append(movie.Movie(m["title"], m["year"], m["storyline"], m["poster_image_url"], m["trailer_youtube_url"]))
+
+    return movies
+
+def main():
+    """Application start point."""
+    # Get movies list
+    movies = get_movies('./data/movies.json')
+    # Open movies page
+    app.open_page(movies)
+
+if __name__ == '__main__':
+    main()
